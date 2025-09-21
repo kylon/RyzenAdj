b683daa3f757b322745c8810d1843e4cdb874dec
kylon <3252255+kylon@users.noreply.github.com>
2025-09-21 23:28:27 +0200

    osdep_linux: fix license headers

--- lib/linux/osdep_linux_mem.c
+++ lib/linux/osdep_linux_mem.c
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: LGPL
 /* Copyright (C) 2018-2019 Jiaxun Yang <jiaxun.yang@flygoat.com> */
 /* Access PCI Config Space - libpci */
-
 #include <sys/mman.h>
 #include <pci/pci.h>
 #include <fcntl.h>
--- lib/linux/osdep_linux_smu_kernel_module.h
+++ lib/linux/osdep_linux_smu_kernel_module.h
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: LGPL
 #pragma once
 
 #include "../osdep.h"
